bc32668b900e4044ed4f61bb032d6799b5cc7205
ohamjoseph <ouilyh@gmail.com>
2024-07-14 02:33:04 +0000

    Ajout des section et organisation du code

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+import { enableProdMode } from '@angular/core';
+import {environment} from "./environments/environment";
+
+if (environment.production) {
+  enableProdMode();
+}
